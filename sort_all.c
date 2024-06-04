ecb1b1a43788a664582cd7560e1ac0b5f79a9a3f
Zakariae Mourid <zmourid@e2r4p10.1337.ma>
2024-06-04 17:35:45 +0100

    lessgo

--- sort_all.c
+++ sort_all.c
@@ -26,7 +26,7 @@ void push_a_to_b(t_data **stack_a, t_data **stack_b)
 		if((*stack_a)->value >= array[start] && (*stack_a)->value <= array[end])
 		{
 			op_push(stack_a,stack_b,"pb");
-			if(end < len -1)
+			if(end < len - 1)
 				end++;
 			start++;
 		}
@@ -40,8 +40,35 @@ void push_a_to_b(t_data **stack_a, t_data **stack_b)
 		}
 		else
 			op_rotate(stack_a,"ra");
-		//print_ld(*stack_b,"stack b");
 	}
 	free(array);
 
+}
+void push_to_b(t_data **stack_a, t_data **stack_b)
+{
+	int len = ft_stacksize(*stack_a);
+	int *array = stack_to_array(*stack_a);
+	int start = 0;	
+	int end  = ft_range(len) - 1;
+	if(!stack_a || !*stack_a)
+		return;
+	while(*stack_a)
+	{
+		if((*stack_a)->value <= array[end])
+			op_push(stack_a,stack_b,"pb");
+		else if((*stack_a)->value < array[start])
+		{
+			op_push(stack_a,stack_b,"pb");
+			op_rotate(stack_b,"rb");
+		}
+		else
+		{
+			op_rotate(stack_a,"ra");
+			continue;
+		}
+		if(end < len -1)
+			end++;
+		start++;
+	}
+	//print_ld(*stack_b, "stack b");
 }
